324f59547300653eebe00862edeb6e64b1ba3844
anhlt13 <anhlt@metropolia.fi>
2024-09-30 15:20:27 +0300

    Update function to select 7 countries random

--- func_ingredient_country.py
+++ func_ingredient_country.py
@@ -18,7 +18,7 @@ def ingredient_country():
     if cursor.rowcount >0 :
         for row in result:
             list_country.append(row[0])
-            country1, country2,country3, country4, country5, country6, country7 = list_country
+
 
     return
 
