bd7e295ff614d1bace2eff72aee6b77cf7ae735c
TaysaAbinader <139161332+TaysaAbinader@users.noreply.github.com>
2024-09-30 11:23:12 +0300

    New changes.

--- GameBody.py
+++ GameBody.py
@@ -3,11 +3,6 @@ import random
 
 list_first_countries = ["Finland", "Cambodia", "Canada", "Peru", "Croatia", "South Africa", "Dubai"]
 first_country = random.choice(list_first_countries)
-#ingredients = ingredients_country()
-#country = retrieve_hints(country, level)
-#points = 300
-#hint_level = 1
-greetings = "Congratulations! You found this ingredient! Let's move on!"
 
 
 game_rules = ("On Vaccine Voyage you are a researcher trying to find ingredients for a vaccine "
@@ -23,24 +18,42 @@ print(f"The first infected country is {first_country}.\n")
 
 disease_name = input("What disease are you going to treat? \n")
 
-print(f"Now we start our jorney to fight {disease_name}...\n")
+print(f"Now we start our journey to fight {disease_name}...\n")
+
+
+ingredients = ingredients_country()
+hints = retrieve_hints(country, level)
+points = 300
+hint_level = 1
+greetings = "Congratulations! You found this ingredient! Let's move on!"
 
 
 while points <= 300 and hint_level < 7:
-    country = retrieve_hints(country, level)
+    for hint in hint_levels:
+        game_movement = input("What do you want to do: guess, new hint or quit?").upper
+        if game_movement == "guess":
+            guess = input("This ingredient is in: ")
+            if guess == hint:
+                guess = True
+                hint_level += 1
+                print(greetings)
+            elif guess != hint:
+                guess = False
+                print("Not this time, try again...")
+            elif game_movement == "new hint":
+                new_hint = hint
+            else game_movement == "quit":
+                break()
+    for hint in hints:
+        if hint <= 6:
+            hint = hints
+            points = point_counter()
+        elif hint > 6:
+            hint = muliple_choice
+            points = point_counter()
     game_movement = input("What do you want to do: guess, new hint or quit?").upper
-    if game_movement == "guess":
-        guess = input("This ingredient is in: ")
-        if guess == country:
-            guess = True
-            print(greetings)
-            hint_level += 1
-        if guess != ingredients_country:
-            guess = False
-        if game_movement == "new hint":
-            new_hint = country
-        if game_movement == "quit":
-            break()
+
+
 
 
 
