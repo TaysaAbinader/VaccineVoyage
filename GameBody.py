40110ef728fb678268e873f2644310fbe730ff12
TaysaAbinader <139161332+TaysaAbinader@users.noreply.github.com>
2024-09-30 16:09:47 +0300

    New changes.

--- GameBody.py
+++ GameBody.py
@@ -1,8 +1,7 @@
 import random
-from databaseconnection import connnection
-import func_retrieve_hints_per_level
-
-
+from databaseconnection import connection
+import functions_game
+from functions_game import insert_data_disease_table
 
 list_first_countries = ["Finland", "Cambodia", "Canada", "Peru", "Croatia", "South Africa", "Dubai"]
 first_country = random.choice(list_first_countries)
@@ -24,24 +23,26 @@ disease_name = input("What disease are you going to treat? \n")
 print(f"Now we start our journey to fight {disease_name}...\n")
 
 names = disease_name
-country = ingredients
 ingredients = ingredient_country()
-hints = func_retrieve_hints_per_level()
+country = ingredients
 points = 300
 hint_level = 1
+guess_count = 1
 greetings = "Congratulations! You found this ingredient! Let's move on!"
 
 
 while points > 0 and hint_level < 7:
     for movement in hint_levels:
+        hints = func_retrieve_hints_per_level()
         game_movement = input("What do you want to do: guess, new hint or quit?").upper
         if game_movement == "GUESS":
             guess = input("This ingredient is in: ")
-            if guess == hint:
-                if guess == 1:
+            if guess == hints:
+                if guess == :
                     points += 10
-                    hint_level += 1
-                    print(greetings)
+                hint_level += 1
+                print(greetings)
+                insert_data_disease_table(names, country)
                 else guess != hint:
                     points = point_counter()
                     print("Not this time, try again...")
@@ -49,6 +50,7 @@ while points > 0 and hint_level < 7:
             hints = retrieve_hints(country, level)
         else game_movement == "QUIT":
             break()
+        game_movement = input("What do you want to do: guess, new hint or quit?").upper
 
     for hint in hints:
         if hint <= 6:
@@ -57,7 +59,7 @@ while points > 0 and hint_level < 7:
         elif hint > 6:
             hint = muliple_choice
             points = point_counter()
-    game_movement = input("What do you want to do: guess, new hint or quit?").upper
+
 
 
 
--- func_ingredient_country.py
+++ func_ingredient_country.py
@@ -1,12 +1,4 @@
-import mysql.connector
-connection = mysql.connector.connect(
-        host='127.0.0.1',
-        port= 3306,
-        database='vaccine_voyage',
-        user='root',
-        password='06042004',
-        aoutocommit=True
-)
+
 
 def ingredient_country():
     sql = f"Select name from countries order by rand() limit 7"
